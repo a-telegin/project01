874089f38dee7ebde5d70528dcf1156a7bcf0343
a-telegin <telegin.andrii@gmail.com>
2016-08-15 17:20:46 +0300

    Add files via upload

---
+++ Company.java
@@ -0,0 +1,35 @@
+//package project01
+public class Company{
+	private int profit;
+	private int assets;
+	private String name;
+	private int forbesListIndex;
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	public String getName(){
+		return this.name;
+	}
+	
+	public void setProfit(int profit){
+		this.profit = profit;
+	}
+	public String getProfit(){
+		return this.profit;
+	}
+	
+	public void setAssets(int assets){
+		this.assets = assets;
+	}
+	public String getAssets(){
+		return this.assets;
+	}
+	
+	public void setForbesListIndex(int forbesListIndex){
+		this.assets = assets;
+	}
+	public String getForbesListIndex(){
+		return this.forbesListIndex;
+	}
+}
---
+++ SortCompany.java
@@ -0,0 +1,5 @@
+public class SortCompany{
+	public static void main(String[] args){
+		
+	}
+}
